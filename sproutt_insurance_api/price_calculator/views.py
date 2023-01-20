b36aaba5f5bf2d44587133fecb9b3ecce8629914
Tomer <tomer5672@gmail.com>
2023-01-20 20:35:45 +0200

    modify cache and lock methods in views.py

--- sproutt_insurance_api/price_calculator/views.py
+++ sproutt_insurance_api/price_calculator/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import pandas as pd
 from sproutt_insurance_api.price_calculator.models import Customer
 import os
-from django.core.cache import cache
+from django.core.cache import cache,lock
 
 
 def calculate_price(request):
@@ -12,8 +12,7 @@ def calculate_price(request):
     height = request.POST.get('height')
     weight = request.POST.get('weight')
 
-
-    #TODO change the df names:
+    # TODO change the df names:
     df1 = get_file_as_df('Health Class table.xlsx')
     df2 = get_file_as_df('Rates-table.xlsx')
     factor = df1[df1['term'] == term]['factor'].values[0]
@@ -28,9 +27,15 @@ def calculate_price(request):
 
 def get_file_as_df(file_path):
     file_modified_timestamp = os.path.getmtime(file_path)
-    file_name = os.path.splitext(os.path.basename(file_path))
+    file_name = os.path.splitext(os.path.basename(file_path))[0]
     data_file = cache.get(f'{file_name}{str(file_modified_timestamp)}')
-    if data_file is None:
-        data_file = pd.read_excel(file_path)
-        cache.set(f'{file_name}{str(file_modified_timestamp)}', data_file)
+    last_modified_timestamp_key = f'{file_name}_last_modified'
+    last_modified_timestamp = cache.get(last_modified_timestamp_key)
+    if last_modified_timestamp is None or last_modified_timestamp != file_modified_timestamp or data_file is None:
+        lock_key = f'{file_name}_lock'
+        # Acquire the lock before reading the file
+        with lock(lock_key):
+            data_file = pd.read_excel(file_path)
+            cache.set(file_name, data_file)
+            cache.set(last_modified_timestamp_key, file_modified_timestamp)
     return data_file
