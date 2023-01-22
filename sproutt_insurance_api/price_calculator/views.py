e539cf352924a9980c902829d6bfb25255de55c8
Tomer <tomer5672@gmail.com>
2023-01-22 22:49:59 +0200

    finished price calculate except special cases

--- sproutt_insurance_api/price_calculator/views.py
+++ sproutt_insurance_api/price_calculator/views.py
@@ -3,6 +3,26 @@ import pandas as pd
 from sproutt_insurance_api.price_calculator.models import Customer
 import os
 from django.core.cache import cache
+from numify.numify import numify
+import re
+
+
+def get_coverage_range(rates_table: pd.DataFrame, coverage_amount: int):
+    regex = '\$\d+kKmMbB? - \$\d+[kKmMbB]?'
+    range_list_str = set([col[0] for col in rates_table.columns if re.match(regex, col[0])])
+    for range_str in range_list_str:
+        low_limit, upper_limit = re.match('\$(\d+[kKmMbB]?) - \$(\d+[kKmMbB]?)', range_str).groups()
+        if re.match('^([0-9]*)(\s)?([kKmMbB])$', low_limit):
+            low_limit = numify(low_limit)
+        else:
+            low_limit = int(low_limit)
+        if re.match('^([0-9]*)(\s)?([kKmMbB])$', upper_limit):
+            upper_limit = numify(upper_limit)
+        else:
+            upper_limit = int(upper_limit)
+        if low_limit < coverage_amount < upper_limit:
+            return range_str
+    return None
 
 
 def get_max_column(row, weight):
@@ -14,10 +34,12 @@ def get_max_column(row, weight):
         return results[-1]
     return None
 
+
 def calculate_price(request):
-    term = request.POST.get('term')
-    coverage = request.POST.get('coverage')
-    age = request.POST.get('age')
+    term = int(request.POST.get('term'))
+    # calculate as int not double.
+    coverage = int(request.POST.get('coverage'))
+    age = int(request.POST.get('age'))
     height = request.POST.get('height')
     weight = request.POST.get('weight')
 
@@ -26,23 +48,26 @@ def calculate_price(request):
     feet, inches = customer.tuple_height
 
     # TODO change the df names:
-    df1 = get_file_as_df(file_path='Health Class table.xlsx', ordering_function=order_health_class_df, skiprows=3)
-    df2 = get_file_as_df(file_path='Rates-table.xlsx', header=[0, 1])
+    health_class_df = get_file_as_df(file_path='Health Class table.xlsx', ordering_function=order_health_class_df,
+                                     skiprows=3)
+    rates_df = get_file_as_df(file_path='Rates-table.xlsx', header=[0, 1])
 
-    relevant_row = df1[(df1['feet'] == feet) & (df1['inches'] == inches)]
+    relevant_row = health_class_df[(health_class_df['feet'] == feet) & (health_class_df['inches'] == inches)]
     relevant_row = relevant_row.drop(columns=['feet', 'inches'])
     health_class_series = relevant_row.apply(lambda row: get_max_column(row, 220), axis=1)
-    if health_class_series:
+    if not health_class_series.empty:
         health_class = health_class_series.iloc[0]
-
-    health_class = df1[df1['term'] == term]['factor'].values[0]
-    # health_class = df2[df2['age'] == age]['health_class'].values[0]
-    # Calculate the price
-    # price = coverage / 1000 * factor
-
-    # Create the quote object
-    # quote = Customer(term=term, coverage=coverage, age=age, height=height, weight=weight, price=price,
-    #                  health_class=health_class)
+        coverage_key = get_coverage_range(rates_table=rates_df, coverage_amount=int(coverage))
+        if coverage_key:
+            rate_row = rates_df[
+                (rates_df['coverage']['age/health-class'] == age) & (rates_df['coverage']['term'] == term)]
+            factor = rate_row[coverage_key][health_class]
+            price = coverage / 1000 * factor
+            # TODO continue
+        else:
+            pass
+    else:
+        pass
 
 
 def get_file_as_df(file_path, ordering_function: callable = lambda *args: None, **kwargs):
