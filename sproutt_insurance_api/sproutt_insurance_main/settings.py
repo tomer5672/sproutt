ae7ef2fd089f86efe2775cfae5aaddcf3e146ef5
Tomer <tomer5672@gmail.com>
2023-01-25 21:22:08 +0200

    finished

--- sproutt_insurance_api/sproutt_insurance_main/settings.py
+++ sproutt_insurance_api/sproutt_insurance_main/settings.py
@@ -11,6 +11,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
